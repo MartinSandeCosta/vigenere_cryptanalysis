60ef94b6910e880662a912785eda2b03c3309e5a
Javier Garea <javigarea@gmail.com>
2020-04-03 23:28:50 +0200

    feat: key_length first approach

--- vigenere.py
+++ vigenere.py
@@ -2,6 +2,10 @@ import argparse
 import sys
 import re
 import hashlib
+from math import ceil, gcd
+from functools import reduce as funcreduce
+from operator import iconcat
+
 
 HELP = """
 usage: vigenere [-h] [-i INPUT] [-d DICTIONARY] [--hash HASH]
@@ -28,6 +32,30 @@ def decipher(string, key, a2i_dict, i2a_dict):
     return ret
 
 
+def count_word_ocurrences(text, length):
+    word_ocurrences = {}
+    max_ocurrences = 0
+    for i in range(len(text)-length):
+        word = text[i:i+length]
+        if word in word_ocurrences:
+            word_ocurrences[word][0] += 1
+            word_ocurrences[word][1].append(i)
+        else:
+            word_ocurrences.update({word: [1, [i]]})
+        if word_ocurrences[word][0] > max_ocurrences:
+            max_ocurrences = word_ocurrences[word][0]
+
+    ocurrences_threshold = ceil(max_ocurrences*0.66)
+
+    return {word: list(map(lambda x, y: y-x, indexes[:-1], indexes[1:]))
+            for (word, [ocurrences, indexes]) in word_ocurrences.items()
+            if ocurrences >= ocurrences_threshold}
+
+
+def list_gcd(int_list):
+    return funcreduce(lambda x, y: gcd(x, y), int_list)
+
+
 def main():
     argparser = argparse.ArgumentParser(prog='vigenere')
     argparser.add_argument('-i', '--input', type=argparse.FileType(), help='input file')
@@ -50,8 +78,20 @@ def main():
     # Hash preprocessing
     input_hash = args.hash.read().replace('\n', '')
 
+    most_ocurrent_words = count_word_ocurrences(input_text, 4)
+    key_length = list_gcd(set(funcreduce(iconcat, most_ocurrent_words.values(), [])))
+    print(f'Key length: {key_length}')
+
+    # Key generation
+    key = 'luz'
+
     # Decrypted text
-    print(decipher(input_text, 'LUZ', a2i_dict, i2a_dict))
+    decrypted_text = decipher(input_text, key, a2i_dict, i2a_dict)
+
+    # Key test
+    test = hashlib.sha256(decrypted_text.encode('utf-8')).hexdigest() == input_hash
+
+    print(f'Key: {key}, Test: {test}')
 
 
 if __name__ == "__main__":
