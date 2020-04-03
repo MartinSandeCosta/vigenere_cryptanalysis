07b91e085bb5ac6d2ef082e99b6940d95d9dea1c
Javier Garea <javigarea@gmail.com>
2020-04-03 20:53:09 +0200

    feat: decipher + test jdp

--- vigenere.py
+++ vigenere.py
@@ -1,5 +1,7 @@
 import argparse
 import sys
+import re
+import hashlib
 
 HELP = """
 usage: vigenere [-h] [-i INPUT] [-d DICTIONARY] [--hash HASH]
@@ -13,6 +15,19 @@ optional arguments:
   --hash HASH           file with the hash to be used for comparison
 """
 
+
+def decipher(string, key, a2i_dict, i2a_dict):
+    """
+    Based on https://github.com/jameslyons/pycipher
+    """
+    key = [k.upper() for k in key]
+    ret = ''
+    for (i, c) in enumerate(string):
+        i = i % len(key)
+        ret += i2a_dict[(a2i_dict[c] - a2i_dict[key[i]]) % len(a2i_dict)]
+    return ret
+
+
 def main():
     argparser = argparse.ArgumentParser(prog='vigenere')
     argparser.add_argument('-i', '--input', type=argparse.FileType(), help='input file')
@@ -22,6 +37,21 @@ def main():
 
     if (not args.input) or (not args.dictionary) or (not args.hash):
         sys.exit(HELP)
+
+    # Dictionaries generation
+    a2i_dict, i2a_dict = {}, {}
+    for (index, value) in enumerate(args.dictionary.read().replace('\n', '')):
+        a2i_dict.update({value: index})
+        i2a_dict.update({index: value})
+
+    # Input text preprocessing
+    input_text = re.sub('[^A-Z]', '', args.input.read().upper())
+
+    # Hash preprocessing
+    input_hash = args.hash.read().replace('\n', '')
+
+    # Decrypted text
+    print(decipher(input_text, 'LUZ', a2i_dict, i2a_dict))
 
 
 if __name__ == "__main__":
